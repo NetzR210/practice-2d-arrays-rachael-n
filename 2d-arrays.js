c45ac46047a6a4c9ac4b70b6371c302a1dc1d335
Rachael <rachaelnetz618@duck.com>
2025-03-23 15:58:41 -0500

    Add inital code

---
+++ 2d-arrays.js
@@ -0,0 +1,24 @@
+// Challenge 1 Student Scores
+
+let allScores = [
+  [84, 70, 76], // Student 1 scores
+  [92, 94, 80], // Student 2 scores
+  [88, 78, 82]  // Student 3 scores
+];
+
+
+// Challenge 2 Build a Seating Grid
+
+let theaterSeating = [
+  ["A1", "A2", "A3", "A4"], // Row 1
+  ["B1", "B2", "B3", "B4"], // Row 2
+  ["C1", "C2", "C3", "C4"]  // Row 3
+];
+
+
+// Challenge 3 Form a Multi-Dimensional Contact List
+
+let contactsList = [
+  ["Lisa Coleman", "lisa.coleman@hotmail.com", "555-1234"], // Contact 1
+  ["Matt Fink", "DoctorFink@icloud.com", "555-5678"]        // Contact 2
+];
